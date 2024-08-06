fbce6b7fcd97c9b034f64621c0cd036841e098a7
Subhajit Barman <95461824+SubhajitB05@users.noreply.github.com>
2024-08-06 23:01:30 +0530

    Axios base url updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'https://barmans-dwell-backend.vercel.app';
 // axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
